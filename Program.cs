3207a09735e7be9a78e633cdeb6e9fb30f302603
unknown <adil.erzhan@gmail.com>
2022-12-09 17:49:04 +0300

    Second method

--- Program.cs
+++ Program.cs
@@ -11,3 +11,23 @@ string[] CreateUsersStringArray(int n)
     }
     return array;
 }
+
+string[] CreateStringArray(string[] array, int numberSymbol = 3)
+{
+    int lengthNewArray = 0;
+    for(int i = 0; i < array.Length;i++)
+        if(array[i].Length <= numberSymbol) lengthNewArray++;
+
+    string[] newArray = new string[lengthNewArray];
+
+    for(int i = 0, j = 0; i < array.Length; i++)
+    {
+        if(array[i].Length <= numberSymbol)
+        {
+            newArray[j] = array[i];
+            j++;
+        }
+    }
+    return newArray;
+}
+
