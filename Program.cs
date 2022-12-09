017c88e45586fef2143002e9ca52fa1e3f2693df
unknown <adil.erzhan@gmail.com>
2022-12-09 17:35:06 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа.
+
+
+string[] CreateUsersStringArray(int n)
+{  
+    string[] array = new string[n];
+    for (int i = 0; i < n; i++) 
+    {
+        Console.Write("Введите элемент массива: ");
+        array[i] = Console.ReadLine();            
+    }
+    return array;
+}
